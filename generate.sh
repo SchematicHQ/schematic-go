dcc90056519aadd8b2d1c98fff0f307d898af161
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-09-12 13:38:24 +0000

    SDK regeneration

--- generate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Run the OpenAPI generator
-GO_POST_PROCESS_FILE="gofmt -w" openapi-generator generate -c config.yaml
-
-# Run the MockGen commands
-mockgen -destination=mocks/api_mocks.go -package=mocks github.com/SchematicHQ/schematic-go/api HTTPClient,AccountsAPI,WebhooksAPI,CrmAPI,BillingAPI,CompaniesAPI,EntitlementsAPI,EventsAPI,FeaturesAPI,PlansAPI,PlangroupsAPI
-mockgen -source=schematic.go -destination=mocks/client_mock.go -package=mocks
